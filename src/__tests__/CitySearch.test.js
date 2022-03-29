459cbbacaabc7e07e1a5a1366299701170da9333
Brian A <89669426+Barget20@users.noreply.github.com>
2022-03-29 16:33:16 -0400

    minor fixed, additions
    1) rearranged the order in api.js
    2) added NProgress
    3) added nprogress.css
    4) added getToken, checkToken into api.js

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -15,8 +15,7 @@ describe("<CitySearch /> component", () => {
         const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find(".city")).toHaveLength(1);
     });
-    });
-
+    
     test("renders a list of suggestions", () => {
         const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
@@ -58,13 +57,13 @@ describe("<CitySearch /> component", () => {
         expect(CitySearchWrapper.find('.suggestions').prop('style')).not.toEqual({ display: 'none' });
     });
 
-    test("selecting a suggestion should hid the sugggestion list", () => {
+    test("selecting a suggestion should hide the sugggestion list", () => {
         CitySearchWrapper.setState({
             query: 'Berlin',
-            showSuggestions: undefined,
+            showSuggestions: undefined
         });
         CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
         expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
-        expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none', });
+        expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none' });
     });
 });
