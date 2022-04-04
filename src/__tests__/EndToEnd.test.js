1e595e9b11bdee5a7a1bd4e115d1708a62bae90a
Brian A <89669426+Barget20@users.noreply.github.com>
2022-04-04 18:14:27 -0400

    Minor additions
    1) minor fix to EndToEnd.test on lines 27, and 34
    2) added describe 'filter events by city' along with test to do this
    3) Created some empty files

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -24,15 +24,14 @@ describe('show/hide an event details', () => {
     });
 
     test('User can expand an event to see its details', async () => {
-        await page.click('.event .details-btn');
+        await page.click('.event .show-details');
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeDefined();
     });
 
-
     test('User can collapse an event to hide its details', async () => 
     {
-        await page.click('.event .details-btn');
+        await page.click('.event .show-details');
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
     });
@@ -40,5 +39,44 @@ describe('show/hide an event details', () => {
     afterAll(() => {
         browser.close();
     });
+});
 
+describe("Filter events by city", () =>
+{
+    let browser;
+    let page;
+    
+    beforeAll(async () => {
+        jest.setTimeout(80000);
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250,
+            ignoreDefaultArgs: ['--disable-extensions']
+        });
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
+
+    test("Events list of all cities will be shown", async () => 
+    {
+        const countEvents = await page.$$.eval('.event', (element) => element.length);
+        expect(countEvents).toBe(2);
+    });
+
+    test('Input text will show suggestions', async () =>
+    {
+        await page.type(".city", "Berlin")
+        const countCities = await page.$$eval('.suggestions li', (element) => element.length);
+        expect(countCities).toBe(2);
+    });
+
+    test('User clicks on suggested city from the list', async () => 
+    {
+        await page.reload();
+        await page.type(".city", "Berlin");
+        await page.click(".suggestion li");
+        const countEvents = await page.$$eval('.event', (element) => element.length);
+        expect(countEvents).toBe(1);
+    });
 });
