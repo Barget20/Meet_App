3c99ef1290f7f1d92a36537864c957178cc0cf15
Brian A <89669426+Barget20@users.noreply.github.com>
2022-04-12 17:24:20 -0400

    Updated Icons & Service Worker
    1) updated icons for meet app in index.html, manifest.json, and adding files to public folder
    2) attempted to register Service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './service-worker';
 import * as atatus from 'atatus-spa';
+
+import reportWebVitals from './reportWebVitals';
 atatus.config('b239c0fa1cb242bb844ea779d8daf276').install();
 
 
@@ -18,13 +19,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-//Never figured out how to do this below 
-atatus.notify(new Error('Test Atatus Setup'));
